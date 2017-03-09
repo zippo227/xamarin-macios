3f83e268f75a29076c6eeda4e59bd36f8869687b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-03-09 12:38:47 +0100

    [tests][sampletester] Avoid symlinks. Works around bug #53185.
    https://bugzilla.xamarin.com/show_bug.cgi?id=53185

--- tests/sampletester/Test.cs
+++ tests/sampletester/Test.cs
@@ -125,7 +125,7 @@ public abstract class SampleTester
 			// restored to location the projects don't expect.
 			// So instead clone the sample repositories into /tmp
 			//return Path.Combine (Path.GetDirectoryName (System.Reflection.Assembly.GetExecutingAssembly ().Location), "repositories");
-			return Path.Combine ("/tmp/xamarin-macios-sample-builder/repositories");
+			return Path.Combine ("/private/tmp/xamarin-macios-sample-builder/repositories");
 		}
 	}
 
