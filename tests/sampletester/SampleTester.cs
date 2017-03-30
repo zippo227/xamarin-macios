cf05c0f4bbfa63001c66fba654be5ebb85e1978e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-03-30 16:23:37 +0200

    [sampletester] Test the mac-samples repo as well.

--- tests/sampletester/SampleTester.cs
+++ tests/sampletester/SampleTester.cs
@@ -200,6 +200,28 @@ xamarin-macios/tests/sampletester/bin/Debug/repositories/mac-ios-samples/SceneKi
 	}
 }
 
+
+public class MacSampleTester : SampleTester
+{
+	const string REPO = "mac-samples";
+	public MacSampleTester ()
+			: base (REPO)
+	{
+	}
+
+	static string [] GetSolutions ()
+	{
+		return GetSolutionsImpl (REPO);
+	}
+
+	protected override Dictionary<string, string> GetIgnoredSolutionsImpl ()
+	{
+		return new Dictionary<string, string>
+		{
+		};
+	}
+}
+
 public class XamarinFormsSampleTester : SampleTester
 {
 	const string REPO = "xamarin-forms-samples";
